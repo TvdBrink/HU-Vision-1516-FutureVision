6dc85b7a0d426e4a492eec08d4d45ca01c0141aa
Jeroen Steendam <jeroensteendam@gmail.com>
2016-04-16 18:59:33 +0200

    Updated gitignore file
    Added ignore for *.opendb files

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -18,35 +18,29 @@ int main(int argc, char * argv[]) {
 	ImageFactory::setImplementation(ImageFactory::DEFAULT);
 	//ImageFactory::setImplementation(ImageFactory::STUDENT);
 
-
-	ImageIO::debugFolder = "D:\\Users\\Rolf\\Downloads\\FaceMinMin";
+	ImageIO::debugFolder = "C:\\Users\\Jeroen\\Documents\\HU-Vision-1516-FutureVision\\testdebug";
 	ImageIO::isInDebugMode = true; //If set to false the ImageIO class will skip any image save function calls
 
-
-
-
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("D:\\Users\\Rolf\\Downloads\\TestA5.jpg", *input)) {
+	if (!ImageIO::loadImage("C:\\Users\\Jeroen\\Documents\\GitHub\\HU-Vision-1516-FutureVision\\testsets\\Set A\\TestSet Images\\female-3.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
 	}
 
-
 	ImageIO::saveRGBImage(*input, ImageIO::getDebugFileName("debug.png"));
 
-	DLLExecution * executor = new DLLExecution(input);
+	DLLExecution executor(input);
 
 
-	if (executeSteps(executor)) {
+	if (executeSteps(&executor)) {
 		std::cout << "Face recognition successful!" << std::endl;
 		std::cout << "Facial parameters: " << std::endl;
 		for (int i = 0; i < 16; i++) {
-			std::cout << (i+1) << ": " << executor->facialParameters[i] << std::endl;
+			std::cout << (i+1) << ": " << executor.facialParameters[i] << std::endl;
 		}
 	}
 
-	delete executor;
 	system("pause");
 	return 1;
 }
