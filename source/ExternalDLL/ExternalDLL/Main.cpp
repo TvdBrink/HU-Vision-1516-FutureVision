acce2e65f3bc39b806af50ba75d60c6b1e085f08
Jeroen Steendam <jeroensteendam@gmail.com>
2016-05-06 16:40:27 +0200

    Paths are now relative

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -15,14 +15,14 @@ bool executeSteps(DLLExecution * executor);
 
 int main(int argc, char * argv[]) {
 
-	ImageFactory::setImplementation(ImageFactory::DEFAULT);
-	//ImageFactory::setImplementation(ImageFactory::STUDENT);
+	//ImageFactory::setImplementation(ImageFactory::DEFAULT);
+	ImageFactory::setImplementation(ImageFactory::STUDENT);
 
-	ImageIO::debugFolder = "C:\\Users\\Jeroen\\Documents\\HU-Vision-1516-FutureVision\\testdebug";
+	ImageIO::debugFolder = "..\\..\\..\\testdebug";
 	ImageIO::isInDebugMode = true; //If set to false the ImageIO class will skip any image save function calls
 
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("C:\\Users\\Jeroen\\Documents\\GitHub\\HU-Vision-1516-FutureVision\\testsets\\Set A\\TestSet Images\\female-3.png", *input)) {
+	if (!ImageIO::loadImage("..\\..\\..\\testsets\\Set A\\TestSet Images\\female-3.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
